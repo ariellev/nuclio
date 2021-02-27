588b351753e17d0158e8c467042e2bba6335a24b
Ariel Lev <ariel.lev@mhp.com>
2021-02-27 15:17:04 +0100

    add handler

--- nuclio-functions/mqtt/main.py
+++ nuclio-functions/mqtt/main.py
@@ -1,2 +1,11 @@
 def handler(context, event):
-    return ""
+    response_body = f'Got {event.method} to {event.path} with "{event.body}"'
+
+    # log with debug severity
+    context.logger.debug('This is a debug level message')
+
+    # just return a response instance
+    return context.Response(body=response_body,
+                            headers=None,
+                            content_type='text/plain',
+                            status_code=201)
